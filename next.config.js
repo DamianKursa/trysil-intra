2a7e0e0621685ff8d9472c030827732061db90e3
DamianKursa <damian.kursa.web@gmail.com>
2025-03-19 21:42:51 +0100

    adding manifest

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-const withPWA = require('next-pwa')({ dest: 'public' });
+// next.config.js
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  // Optional: disable in development
+  disable: process.env.NODE_ENV === 'development'
+});
 
 module.exports = withPWA({
   images: {
-    domains: ['secure.gravatar.com'],
-  },
+    domains: ['secure.gravatar.com'] // your external image domains, if any
+  }
 });
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,59 +1,60 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+// pages/_document.tsx
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
-    <Html lang="pl-PL">
+    <Html lang='pl-PL'>
       <Head>
         {/* Meta Tags */}
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta
-          name="description"
-          content="Odkryj nowoczesne gałki, uchwyty meblowe, klamki i wieszaki. Stylowe akcesoria wnętrzarskie od HVYT dla wymagających klientów."
+          name='description'
+          content='Trysil Intranet – Bezpieczny system wewnętrzny organizacji. Zarządzaj użytkownikami, wydarzeniami i treściami w nowoczesnym interfejsie.'
         />
         <meta
-          name="keywords"
-          content="uchwyty meblowe, gałki meblowe, klamki, wieszaki, akcesoria wnętrzarskie, HVYT, stylowe wnętrza"
+          name='keywords'
+          content='Trysil, Intranet, system wewnętrzny, zarządzanie, organizacja, bezpieczeństwo'
         />
-        <meta name="author" content="HVYT" />
-        <meta name="robots" content="index, follow" />
-        <link rel="canonical" href="https://hvyt.pl" />
+        <meta name='author' content='Trysil RMM' />
+        <meta name='robots' content='index, follow' />
+        <link rel='canonical' href='https://trysil-intra.vercel.app' />
 
         {/* Open Graph Meta Tags for Social Media */}
         <meta
-          property="og:title"
-          content="HVYT – Nowoczesne Gałki i Uchwyty Meblowe"
+          property='og:title'
+          content='Trysil Intranet – System zarządzania wewnętrznego'
         />
         <meta
-          property="og:description"
-          content="Stylowe akcesoria wnętrzarskie: gałki, uchwyty, klamki i wieszaki. Odkryj nasze kolekcje!"
+          property='og:description'
+          content='Trysil Intranet to nowoczesne rozwiązanie do zarządzania treściami, użytkownikami oraz wydarzeniami w Twojej organizacji.'
         />
-        <meta property="og:url" content="https://hvyt.pl" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://hvyt.pl/og-image.jpg" />
+        <meta property='og:url' content='https://trysil-intra.vercel.app' />
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:image'
+          content='https://trysil-intra.vercel.app/og-image.jpg'
+        />
 
         {/* Favicon */}
-        <link rel="icon" href="/favicon.png" />
+        <link rel='icon' href='/favicon.png' />
 
         {/* Fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='anonymous'
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
-          rel="stylesheet"
+          href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap'
+          rel='stylesheet'
         />
-
-        {/* Additional Resources */}
-        <link rel="stylesheet" href="https://hvyt.pl/styles.css" />
       </Head>
-      <body className="bg-beige-light">
+      <body className='bg-beige-light'>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
